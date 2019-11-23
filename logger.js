fbc156361758dd677862212d48876d59936685a8
Peter Jenkin <peternjenkin@gmail.com>
2019-11-23 14:39:31 +0000

    Typo

--- logger.js
+++ logger.js
@@ -1,6 +1,6 @@
 // 7-141 Development with SAM - Creating Lambda Functions
 // WTF is he doing? He hasn't said.
-// I think these are Lambda funcions' code to be used with SAM later
+// I think these are Lambda functions' code to be used with SAM later
 exports.handler = async (event) =>
 {
     return {
