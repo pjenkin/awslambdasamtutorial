b2055519aabb6874c8181e9e68398587b80946f5
Peter Jenkin <peternjenkin@gmail.com>
2019-11-23 15:43:24 +0000

    7-142 Creating first SAM template - Functions with API methods and Swagger, and an S3 bucket resource, defined in YAML

--- logger.js
+++ logger.js
@@ -1,10 +1,16 @@
 // 7-141 Development with SAM - Creating Lambda Functions
 // WTF is he doing? He hasn't said.
 // I think these are Lambda functions' code to be used with SAM later
+
+
 exports.handler = async (event) =>
 {
+// 7-142 First SAM template - log PUT'ing into S3 (as PUT is asynchronous, no response would normally be triggered)
+    let eventJson = JSON.stringify(event);
+    console.log(eventJson);
+
     return {
         statusCode: 200,
-        body: JSON.stringify(event)
+        body: eventJson
     }
 }
